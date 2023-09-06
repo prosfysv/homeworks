e4e8e9d9f5ee00d5060fa3d25ab701069a0672fc
prosfysv <nightlovel18lovel@gmail.com>
2023-09-07 01:34:37 +0400

    first commit

---
+++ day3/lesson3.py
@@ -0,0 +1,8 @@
+name = "Saba"
+print(len(name))
+full_name = "Saba Jmukhadze"
+print(full_name[4:])
+print(full_name[:4])
+print(full_name.upper())  #ასევე არის lower
+print(name.replace("a", "#"))
+print(full_name.replace("a", "$"))
